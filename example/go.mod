ff78cee590c741a400006a9acb193ddcc207936b
yjp19871013@126.com <yjp19871013@126.com>
2021-07-15 10:46:20 +0800

    修改模块名

--- example/go.mod
+++ example/go.mod
@@ -3,6 +3,5 @@ module eventbus-example
 go 1.16
 
 require (
-	github.com/yjp19871013/event-bus v0.0.1
 	gopkg.in/vrecan/death.v3 v3.0.1
 )
--- example/main.go
+++ example/main.go
@@ -1,9 +1,14 @@
 package main
 
 import (
-	"github.com/yjp19871013/event-bus"
+	"github.com/yjp19871013/event-bus/eventbus"
+	DEATH "gopkg.in/vrecan/death.v3"
+	"syscall"
 )
 
 func main() {
+	bus := eventbus.NewEventBus()
 
+	death := DEATH.NewDeath(syscall.SIGINT, syscall.SIGTERM)
+	_ = death.WaitForDeath()
 }
